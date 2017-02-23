2913c8455249edc00433a18b4e1b1bbda4e94daf
Viktoriya Mikhailova <viktoriya.mikh@namecheap.com>
2017-02-23 16:31:18 +0200

    LowLatencyDemo with GC notifications

--- GarbageCollector/GarbageCollector/Program.cs
+++ GarbageCollector/GarbageCollector/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,6 +13,11 @@ namespace GarbageCollector
     {
         static void Main(string[] args)
         {
+            Console.WriteLine($"MaxGeneration: {GC.MaxGeneration}");
+            Console.WriteLine($"IsServerGC: {GCSettings.IsServerGC}");
+            Console.WriteLine($"IsServerGC: {GCSettings.LatencyMode}");
+
+            GCSettings.LatencyMode = GCLatencyMode.Batch;
 
             // Создание объекта Timer, вызывающего метод TimerCallback
             // каждые 2000 миллисекунд
@@ -25,10 +32,34 @@ namespace GarbageCollector
         }
         private static void TimerCallback(Object o)
         {
+            var startDate = DateTime.Now;
+
             // Вывод даты/времени вызова этого метода
             Console.WriteLine("In TimerCallback: " + DateTime.Now);
             // Принудительный вызов уборщика мусора в этой программе
             GC.Collect();
-    }
+
+            var endDate = DateTime.Now;
+            var totalTime = (endDate - startDate).Milliseconds;
+            if (totalTime > 0) Console.WriteLine($"TotalTime = {totalTime}");
+        }
+
+        private static void LowLatencyDemo()
+        {
+            GCLatencyMode oldMode = GCSettings.LatencyMode;
+
+            System.Runtime.CompilerServices.RuntimeHelpers.PrepareConstrainedRegions();
+            try
+            {
+                GCSettings.LatencyMode = GCLatencyMode.LowLatency;
+                // Здесь выполняется код
+            }
+            finally
+            {
+                GCSettings.LatencyMode = oldMode;
+            }
+        }
+
+
     }
 }
